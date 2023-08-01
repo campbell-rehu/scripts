4183664a8b546e16d87e08987f357abe779d37a9
Campbell <campbellrehu@me.com>
2023-08-01 22:32:37 +1200

    Add update compose projects script

---
+++ update-compose-projects.sh
@@ -0,0 +1,13 @@
+# This script pulls the latest docker image for any number of docker compose projects in separate directories.
+# Limitations:
+# - The docker-compose file must be in the root directory of the project.
+for var in "$@"
+do
+    cd $var
+    echo "pulling latest image for" $var
+    docker-compose pull
+    echo "recreating" $var "using latest image"
+    docker-compose down -v && docker-compose up -d
+    echo -e "new container running, returning to root directory\n"
+    cd ..
+done
