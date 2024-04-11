2e8e25bf75610d55332c076cd7efd3694316bdf1
Campbell <campbellrehu@me.com>
2024-04-11 22:58:22 +1200

    cht.sh: add source

--- cht.sh
+++ cht.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+
+# Based on the script from ThePrimeagen: https://youtu.be/hJzqEAf2U4I?si=25nAdIQ4mt3B6XkU
 set -e
 
 languages=`echo "go js lua rust c cpp haskell csharp" | tr " " "\n"`
